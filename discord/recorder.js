ac557bfc7ead293fc198d37c9b94c285e103ea15
WoodManGitHub <woodmanpublic@gmail.com>
2020-04-14 22:10:53 +0800

    Try to fix all bug

--- discord/recorder.js
+++ discord/recorder.js
@@ -4,43 +4,58 @@ const moment = require('moment-timezone')
 const config = require('../config.json')
 const AudioUtils = require('../utils/audio')
 
-module.exports = connectToVoice
+module.exports = (discordClient) => {
+    const voiceConfig = config.discord.voiceChannels[0]
 
-function connectToVoice(discordClient) {
-    const voiceConfig = config.discord.voiceChannels
+    console.log(`[Discord Voice] Connecting to voice channel => ${voiceConfig.channelID} .`)
 
-    voiceConfig.forEach(channel => {
-        console.log(`[Discord Voice] Connecting to voice channel => ${channel.channelID} .`)
-
-        discordClient.joinVoiceChannel(channel.channelID).catch(err => {
+    const joinVoiceChannel = () => {
+        discordClient.joinVoiceChannel(voiceConfig.channelID).catch(err => {
             console.log(err)
         }).then(connection => {
-            console.log(`[Discord Voice] Connected to voice channel => ${channel.channelID} .`)
+            console.log(`[Discord Voice] Connected to voice channel => ${voiceConfig.channelID} .`)
 
             const mixer = new Mixer(16, 2, 48000)
             const streams = connection.receive('pcm')
             const userMP3Buffer = []
             let users = {}
 
-            connection.on('disconnect', err => {
+            connection.once('disconnect', err => {
                 console.error(err)
                 users = {}
+                connection.removeAllListeners()
+                streams.removeAllListeners()
+                discordClient.removeAllListeners()
+
+                setTimeout(() => {
+                    discordClient.leaveVoiceChannel(voiceConfig.channelID)
+                    joinVoiceChannel()
+                }, 5000);
             })
 
-            discordClient.on('voiceChannelLeave', (member, oldChannel) => {
-                if (users[member.id]) {
-                    users[member.id].end()
+            discordClient.on('voiceChannelSwitch', (member, newChannel, oldChannel) => {
+                if (newChannel != voiceConfig.channelID) {
+                    if (users[member.id]) {
+                        users[member.id].end()
+                        delete users[member.id]
+                    }
+                }
+            })
+
+            connection.on('userDisconnect', userID => {
+                if (users[userID]) {
+                    users[userID].end()
                     delete users[userID]
                 }
             })
 
             streams.on('data', (data, userID) => {
-                if (userID == undefined || channel.record.ignoreUsers.includes(userID)) return
+                if (userID == undefined || voiceConfig.record.ignoreUsers.includes(userID)) return
                 if (!users[userID]) {
-                    console.log(`[Discord Record] Add user ${userID} to record mixer ${channel.channelID} .`)
+                    console.log(`[Discord Record] Add user ${userID} to record mixer ${voiceConfig.channelID} .`)
                     users[userID] = new Stream.PassThrough()
                     mixer.addSource(users[userID])
-                    
+
                     AudioUtils.generatePCMtoMP3Stream(mixer).on('data', mp3Data => {
                         userMP3Buffer.push(mp3Data)
                         if (userMP3Buffer.length > 4096) userMP3Buffer.shift(userMP3Buffer.length - 4096)
@@ -49,14 +64,14 @@ function connectToVoice(discordClient) {
                 users[userID].write(data)
             })
 
-            if (channel.record.sendTo.type == 'telegram' && channel.record.sendTo.chatID) {
+            if (voiceConfig.record.sendTo.type == 'telegram' && voiceConfig.record.sendTo.chatID) {
                 let mp3File = []
-    
+
                 AudioUtils.generatePCMtoMP3Stream(mixer).on('data', data => {
                     mp3File.push(data)
                 })
-    
-                setInterval(() => {
+
+                const intervalId = setInterval(() => {
                     const mp3Start = moment().tz('Asia/Taipei').format('YYYY-MM-DD hh:mm:ss')
                     const mp3End = moment().tz('Asia/Taipei').format('YYYY-MM-DD hh:mm:ss')
                     const caption = `${mp3Start} -> ${mp3End} \n${moment().tz('Asia/Taipei').format('#YYYYMMDD #YYYY')}`
@@ -64,9 +79,16 @@ function connectToVoice(discordClient) {
                     const fileData = Buffer.concat(mp3File)
 
                     mp3File = []
-                    global.telegram.sendAudio(channel.record.sendTo.chatID, fileData, fileName, caption)
+                    global.telegram.sendAudio(voiceConfig.record.sendTo.chatID, fileData, fileName, caption)
                 }, 20 * 1000)
+
+                connection.once('disconnect', err => {
+                    clearInterval(intervalId)
+                })
             }
         })
-    })
+
+    }
+
+    joinVoiceChannel()
 }
